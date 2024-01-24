bb35dc34506647c0c3a64f3024fb4ccb8e781dcd
Will Ruggiano <wmruggiano@gmail.com>
2024-01-23 21:47:24 -0700

    fix(v1.86.0): pin connor4312/picomatch

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
 {
   config,
   dream2nix,
+  lib,
   ...
 }: {
   imports = [
@@ -18,10 +19,19 @@
   deps = {nixpkgs, ...}: {
     inherit
       (nixpkgs)
+      fetchFromGitHub
       libsecret
       pkg-config
       stdenv
       ;
+
+    # https://github.com/microsoft/vscode-js-debug/commit/f33da847503857454d2abc6f35e72a9722115b46
+    picomatch = nixpkgs.fetchFromGitHub {
+      owner = "connor4312";
+      repo = "picomatch";
+      rev = "2fbe90b12eafa7dde816ff8c16be9e77271b0e0b";
+      hash = "sha256-NWVzzTlGfyXG/N0z7wy3oZDjuGZ1uQHfwT/EEKGp73Q=";
+    };
   };
 
   nodejs-granular-v3 = {
@@ -30,6 +40,11 @@
         mkDerivation = {
           nativeBuildInputs = [config.deps.pkg-config];
           buildInputs = [config.deps.libsecret];
+        };
+      };
+      picomatch."2.3.1" = {
+        mkDerivation = {
+          src = lib.mkForce config.deps.picomatch;
         };
       };
       playwright."1.26.0" = {
